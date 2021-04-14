03393bab155f962aae4a3ebad396fdf45a56e78c
Adan <ajpalacios.sist@gmail.com>
2021-04-14 10:37:07 -0500

    Initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from "react-router-dom";
+
 import './index.css';
 import App from './App';
+import configureStore from "./store/configureStore";
 import reportWebVitals from './reportWebVitals';
+import ErrorBoundary from "./components/ErrorBoundary"
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={configureStore()}>
+    <BrowserRouter>
+      <ErrorBoundary>{[<App key="App" />]}</ErrorBoundary>
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
